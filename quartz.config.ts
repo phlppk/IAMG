6c90e54e13e8bd7daf4f3969eaf53f321b44d4b2
#phlppk <nospam-github.com@koehler-philipp.de>
2023-10-07 04:53:25 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "atem.reise",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
