04a16115eac6cd963ce11c5fc918974530514d80
#phlppk <nospam-github.com@koehler-philipp.de>
2023-10-11 13:19:30 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Philipp Paul Köhler": "https://koehler-philipp.de",
+      "Telegram Gruppe": "https://t.me/+UiwrwcJsBgMyZWQy",
     },
   }),
 }
